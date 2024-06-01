b8d808d31dc5478b595877c65188c88bd2bd5b92
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-06-01 16:48:20 +0600

    Added user verification and user info database collection

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import cors from 'cors';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const app = express();
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+
+const port = process.env.PORT || 7000;
+const uri = `mongodb+srv://${process.env.DATABASE_USERNAMe}:${process.env.DATABASE_PASSWORD}@cluster0.fp7vkua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(cors())
+
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // DATABASE COLLECTION
+        const allUserCollection = client.db("ANT").collection("All-users")
+
+        // SAVE USER INFORMATION
+        app.post("/user-info", async(req,res)=>{
+            const allInfo = req.body;
+            const result = await allUserCollection.insertOne(allInfo);
+            res.send(result);
+        })
+        // SEND USER DATA 
+        app.get("/user-info/:email", async (req,res)=>{
+            const query = {email : req.params?.email};
+            const result = await allUserCollection.findOne(query);
+            res.send(result);
+        })
+
+        // SEND USER SECRET 
+        app.post("/verify-user",async(req,res)=>{
+            const email = req.body;
+            const token = jwt.sign(email, process.env.USER_SECRET,{expiresIn:"2h"});
+            res.send({token:token});
+        })
+
+        //-----TEST API
+        app.get("/",(req,res)=>{
+            res.send("Server is running on the way.......")
+        })
+    } finally {
+        
+    }
+}
+run().catch(console.dir);
+
+app.listen(port,()=>{
+    console.log("server is running......")
+})
+
